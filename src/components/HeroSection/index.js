fd357711902f30889acb6cf3b0b3ba75482ed06f
Jim <jsmobst@gmail.com>
2023-03-25 20:42:49 -0500

    F HER KING

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Suspense, useEffect } from 'react';
 import Video from './videos/iStock_optic.mp4';
 import {
   HeroContainer,
@@ -10,10 +10,14 @@ import {
   HeroBtnWrapper,
   TButton,
 } from './HeroElements';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Text } from '@react-three/drei'
+import * as THREE from 'three'
+import Model from '../3d_models/brain'
+import { OrbitControls } from "@react-three/drei";
 
 
-
-function HeroSection() {
+export default function HeroSection() {
   const [hover, setHover] = useState(false);
   const onHover = () => {
     setHover(!hover);
@@ -24,9 +28,22 @@ function HeroSection() {
         <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
       </HeroBg>
       <HeroContent>
-        <AnimatedGradientText>
-        Leibniz Analytica
-        </AnimatedGradientText>
+      <Canvas
+    shadows
+    gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
+    camera={{ position: [0, 0, 20], fov: 32.5, near: 1, far: 100 }}
+    onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}>      
+      <fog attach="fog" args={['black', 15, 20]} />
+      <Suspense fallback={null}>
+        <group position={[0, -1, 0]}>
+          <VideoText position={[0, 1.3, -2]} />
+        </group>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[0, 10, 0]} intensity={0.3} />
+        <directionalLight position={[-50, 0, -40]} intensity={0.7} />
+        <Intro />
+      </Suspense>
+    </Canvas>
         <HeroP>
           Artificial Intelligence.
         </HeroP>
@@ -99,4 +116,26 @@ function HeroSection() {
   );
 }
 
-export default HeroSection;
+
+function VideoText(props) {
+  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
+  useEffect(() => void video.play(), [video])
+  return (
+    <Text fontSize={3} letterSpacing={-0.06} {...props}>
+      LEIBNIZ
+      <meshBasicMaterial toneMapped={false}>
+        <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
+      </meshBasicMaterial>
+    </Text>
+  )
+}
+
+
+
+function Intro() {
+  const [vec] = useState(() => new THREE.Vector3())
+  return useFrame((state) => {
+    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05)
+    state.camera.lookAt(0, 0, 0)
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,21 +28,19 @@ export default function Welcome() {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
-      <Canvas concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 100], fov: 15 }}>
+      {/* <Canvas concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 100], fov: 15 }}>
       <color attach="background" args={['black']} />
       <fog attach="fog" args={['black', 15, 20]} />
       <Suspense fallback={null}>
         <group position={[0, -1, 0]}>
-          {/* <Carla rotation={[0, Math.PI - 0.4, 0]} position={[-1.2, 0, 0.6]} scale={[0.26, 0.26, 0.26]} /> */}
           <VideoText position={[0, 1.3, -2]} />
-          {/* <Ground /> */}
         </group>
         <ambientLight intensity={0.5} />
         <spotLight position={[0, 10, 0]} intensity={0.3} />
         <directionalLight position={[-50, 0, -40]} intensity={0.7} />
         <Intro />
       </Suspense>
-    </Canvas>
+    </Canvas> */}
       <InfoSection {...homeObjOne} />
 
       
