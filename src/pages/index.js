4c3d712dd1a443e97192229906a4ccc4db37f63e
Jim <jsmobst@gmail.com>
2023-04-03 11:13:49 -0500

    LESS

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,7 @@ import {
 import { Canvas, useFrame, extend, useThree, createPortal } from '@react-three/fiber'
 import { Text, Cylinder, meshBounds, Line, Stage, useFBO, useVideoTexture, useAspect, useTexture, useIntersect, Image, ScrollControls, Scroll, shaderMaterial } from '@react-three/drei'
 import * as THREE from 'three'
-import Scenemodel from '../components/3d_models/Scene_draco'
-import Brainmodel from '../components/3d_models/brain'
-import { OrbitControls, useGLTF } from "@react-three/drei";
-import { a } from '@react-spring/web'
-import Model from '../components/GEO/Geo'
+
 import state from '../components/GEO/state';
 import { Block, useBlock } from "../components/GEO/blocks";
 import { useDrag } from "@use-gesture/react"
@@ -95,63 +91,7 @@ function FadingImage() {
 }
 
 
-function Scene() {
-  const size = useAspect(1800, 1000)
-  return (
-    <mesh scale={size}>
-      <planeGeometry />
-      <Suspense fallback={<FallbackMaterial url="c4cA8UN.jpg" />}>
-        <VideoMaterial url="drei.mp4" />
-      </Suspense>
-    </mesh>
-  )
-}
 
-function Item({ url, scale, ...props }) {
-  const visible = useRef(false)
-  const [hovered, hover] = useState(false)
-  const ref = useIntersect((isVisible) => (visible.current = isVisible))
-  const { height } = useThree((state) => state.viewport)
-  useFrame((state, delta) => {
-    ref.current.position.y = THREE.MathUtils.damp(ref.current.position.y, visible.current ? 0 : -height / 2 + 1, 4, delta)
-    ref.current.material.zoom = THREE.MathUtils.damp(ref.current.material.zoom, visible.current ? 1 : 1.5, 4, delta)
-    ref.current.material.grayscale = THREE.MathUtils.damp(ref.current.material.grayscale, hovered ? 0 : 1, 4, delta)
-  })
-  return (
-    <group {...props}>
-      <Image ref={ref} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} scale={scale} url={url} />
-    </group>
-  )
-}
-
-function Items() {
-  const { width: w, height: h } = useThree((state) => state.viewport)
-  return (
-    <Scroll>
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 6, 0, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[2, w / 3, 1]} position={[w / 30, -h, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 1, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 5, w / 5, 1]} position={[w / 4, -h * 1.2, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 5, w / 5, 1]} position={[w / 10, -h * 1.75, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 4, -h * 2, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 2.6, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 2, w / 2, 1]} position={[w / 4, -h * 3.1, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 2.5, w / 2, 1]} position={[-w / 6, -h * 4.1, 0]} />
-    </Scroll>
-  )
-}
-
-
-
-function VideoMaterial({ url }) {
-  const texture = useVideoTexture(url)
-  return <meshBasicMaterial map={texture} toneMapped={false} />
-}
-
-function FallbackMaterial({ url }) {
-  const texture = useTexture(url)
-  return <meshBasicMaterial map={texture} toneMapped={false} />
-}
 
 
 
@@ -174,30 +114,7 @@ export default function Welcome() {
 
       
         <InfoSection3 {...homeObjThree} /> 
-        {/* <Footer/> */}
-        {/* <HeroContainerCanva > 
-        <Canvas orthographic camera={{ zoom: 80 }} gl={{ alpha: false, antialias: false, stencil: false, depth: false }} dpr={[1, 1.5]}>
-
-        <color attach="background" args={['#181818']} />
-    <ScrollControls damping={6} pages={5}>
-      <Items />
-      <Scroll html style={{ width: '100%' }}> */}
-
-        {/* <h1 style={{ position: 'absolute', top: `100vh`, right: '20vw', fontSize: '25em', transform: `translate3d(0,-100%,0)` }}>all</h1>
-        <h1 style={{ position: 'absolute', top: '180vh', left: '10vw' }}>hail</h1>
-        <h1 style={{ position: 'absolute', top: '260vh', right: '10vw' }}>thee,</h1>
-        <h1 style={{ position: 'absolute', top: '350vh', left: '10vw' }}>thoth</h1>
-        <h1 style={{ position: 'absolute', top: '450vh', right: '10vw' }}>
-          her
-          <br />
-          mes.
-        </h1> */}
-
-      {/* </Scroll>
-    </ScrollControls>
-    </Canvas>
-
-    </HeroContainerCanva >  */}
+ 
 
     </>
   );
