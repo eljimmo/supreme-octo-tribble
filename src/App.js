b880cf59ffab82f578118f437d446661c34a78ca
blackalien <jsmobst@gmail.com>
2023-04-06 21:33:34 -0500

    Demo 666666

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { Account } from "./pages/Account";
 import Welcome from "./pages/index";
 import React from "react";
-import Dashboard from "./views/Dashboard";
+// import Dashboard from "./views/Dashboard";
 import StockSearch from "./views/StockSearch/Stock_Search_Index";
 
 
