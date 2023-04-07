6249d11063308765bb4046923414548ed4205bd3
blackalien <jsmobst@gmail.com>
2023-04-07 13:29:15 -0500

    more content added

--- src/views/StockSearch/Stock_Search_Index.js
+++ src/views/StockSearch/Stock_Search_Index.js
@@ -1,8 +1,8 @@
 import useAppState from './hooks/useAppState';
 import Searchbar from './SSCompos/SearchBar/SE_Index';
 import Content from './SSCompos/Content/Output/ContentIndex';
-import Content3 from './SSCompos/Content/Output/3content';
-import Content2 from './SSCompos/Content/Output/2Content';
+// import Content3 from './SSCompos/Content/Output/3content';
+// import Content2 from './SSCompos/Content/Output/2Content';
 import Spinner from './SSCompos/Spinner/Index';
 
 const StockSearch = () => {
@@ -12,8 +12,8 @@ const StockSearch = () => {
     <div style={{padding: 20}} >
       <Searchbar searchSubmit={handleSearch} />
       <main>{isLoading ? <Spinner /> : <Content data={symbolData} />}</main>
-      <main>{isLoading ? <Spinner /> : <Content3 data={symbolData} />}</main>
-      <main>{isLoading ? <Spinner /> : <Content2 data={symbolData} />}</main>
+      {/* <main>{isLoading ? <Spinner /> : <Content3 data={symbolData} />}</main>
+      <main>{isLoading ? <Spinner /> : <Content2 data={symbolData} />}</main> */}
 
     </div>
   );
