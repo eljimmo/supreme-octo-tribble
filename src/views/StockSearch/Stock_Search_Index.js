972ac1e3a6bfc5c96c000b904ffa529b395cbbaf
blackalien <jsmobst@gmail.com>
2023-04-07 09:17:27 -0500

    more content

--- src/views/StockSearch/Stock_Search_Index.js
+++ src/views/StockSearch/Stock_Search_Index.js
@@ -1,6 +1,8 @@
 import useAppState from './hooks/useAppState';
 import Searchbar from './SSCompos/SearchBar/SE_Index';
 import Content from './SSCompos/Content/Output/ContentIndex';
+import Content3 from './SSCompos/Content/Output/3content';
+import Content2 from './SSCompos/Content/Output/2Content';
 import Spinner from './SSCompos/Spinner/Index';
 
 const StockSearch = () => {
@@ -10,6 +12,9 @@ const StockSearch = () => {
     <div >
       <Searchbar searchSubmit={handleSearch} />
       <main>{isLoading ? <Spinner /> : <Content data={symbolData} />}</main>
+      <main>{isLoading ? <Spinner /> : <Content3 data={symbolData} />}</main>
+      <main>{isLoading ? <Spinner /> : <Content2 data={symbolData} />}</main>
+
     </div>
   );
 };
