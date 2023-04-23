813dfd9238aaba9386cddafe3543e64e5e6ed931
blackalien <jsmobst@gmail.com>
2023-04-23 17:15:37 -0500

    h9omage to Leibniz 2

--- src/components/FrostedGlass/Overlay.js
+++ src/components/FrostedGlass/Overlay.js
@@ -2,6 +2,7 @@ import { Children } from 'react'
 import { motion } from 'framer-motion'
 import { Logo } from '@pmndrs/branding'
 import { useStore } from './store'
+import Typewriter from 'typewriter-effect';
 
 const container = {
   hidden: { opacity: 0, height: 0, transition: { staggerChildren: 0.05 } },
@@ -37,11 +38,24 @@ export function Overlay() {
         <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>
         Project Leibniz Analytica
           <br />
-          {/* Project Leibniz Analytica */}
-        </a>
+          <Typewriter
+options={{
+strings: ['Creating Intelligence', 'Creating Agency', 'Creating Consciousness', 'Creating Solutions'],
+autoStart: true,
+loop: true,
+}}
+/>        </a>
         <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>
-          {/* 04/32/2023 */}
-          </div>
+
+        <Typewriter
+options={{
+strings: ['Created by the Project Leibniz Analytica Team', 'Releasing 2024', 'Creating Solutions', 'Creating Intelligence'],
+autoStart: true,
+loop: true,
+}}
+/> 
+        
+        </div>
       </div>
       <Logo style={{ position: 'absolute', top: 40, left: 40, width: 30 }} />
       <div className="info">
@@ -51,6 +65,13 @@ export function Overlay() {
 
         </h1>
         <List open={state.open}>
+          {/* <h3>Lorem ipsum</h3>
+          <h3>Lorem ipsum</h3> */}
+          <h3>
+            {/* <span className="accent">Lorem ipsum</span> */}
+          </h3>
+          {/* <h4>Lorem ipsum</h4> */}
+          {/* <p className="price">$Lorem ipsum</p> */}
           <p>
             Named after Gottfried Wilhelm Leibniz the Father of Calculus, Project Leibniz Analytica is a project that aims to create a platform to build AI models and Agents for Financial Analysis.
             Neuro Evolution Algorithms are a type of Artificial Intelligence Process that is founded on the concept of biology and evolution.
