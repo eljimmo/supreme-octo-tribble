77368de95a9fad87ee0568b67ca95540eb623ef5
blackalien <jsmobst@gmail.com>
2023-04-27 15:01:43 -0500

    leibniz 58

--- src/pages/Home_page_index.js
+++ src/pages/Home_page_index.js
@@ -1,33 +1,29 @@
 import React, { useState } from 'react';
-import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 
 import {
   homeObjOne,
-  homeObjTwo,
-  homeObjThree,
-  homeObjFive,
+  // homeObjTwo,
+  // homeObjThree,
+  // homeObjFive,
 
 
 } from '../components/InfoSection/Data';
-import { Affix, Button, Text, Transition, rem } from '@mantine/core';
-import HeroSection_2 from '../components/HeroSection/HeroIndex_2';
+import { Affix, Button, rem } from '@mantine/core';
+// import HeroSection_2 from '../components/HeroSection/HeroIndex_2';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer/index';
-import DesignSection from '../components/DesignSection';
-import VideoSection from '../components/Videos_Section';
+// import DesignSection from '../components/DesignSection';
+// import VideoSection from '../components/Videos_Section';
 
 import ScApp from '../components/ScrollControl/src/App';
 // import { HeroContainerCanva, HeroContainer, HeroContainer200, HeroContainerTransparent, HeroContainer400, HeroContent2, HeroContent3, HeroContent }  from "../components/HeroSection/HeroElements";
-import Neuro_page from '../components/ScrollControl/src/Neuro_Evo';
 import './pages.css'
 import Sidebar from '../components/Sidebar/index';
-import { HeroContainer, HeroContainerTransparent } from '../components/HeroSection/HeroElements';
+import { HeroContainerTransparent } from '../components/HeroSection/HeroElements';
 import Kamdo_App from '../components/Kamdo/index';
-import K_App from '../components/Kamdo/App';
 import Frosted_App from '../components/FrostedGlass/index';
-import InfoSection6 from '../components/InfoSection/index_Infosection4';
-import P_App from '../components/Perfomance/index_OVERLAY';
+// import InfoSection6 from '../components/InfoSection/index_Infosection4';
 import StockSearch from '../views/StockSearch/Stock_Search_Index';
 
 
