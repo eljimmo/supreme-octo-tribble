f523aec1b15f43ef94f015bcd7f09bfe2a50569f
blackalien <jsmobst@gmail.com>
2023-04-28 15:43:47 -0500

    leibniz 888

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -15,7 +15,7 @@ import {
   Img
 } from './InfoElements';
 // import { Canvas } from '@react-three/fiber'
-import { useAspect, useVideoTexture, useTexture } from '@react-three/drei'
+// import { useAspect, useVideoTexture, useTexture } from '@react-three/drei'
 // import Model from '../GEO/Geo'
 // import SombreroSuperficieMath from '../3d_models/Sombrero_superficie_math';
 // import Scenemodel from '../3d_models/Scene_draco'
@@ -24,27 +24,27 @@ import { useAspect, useVideoTexture, useTexture } from '@react-three/drei'
 
 
 
-function Scene() {
-  const size = useAspect(1800, 1000)
-  return (
-    <mesh scale={size}>
-      <planeGeometry />
-      <Suspense fallback={<FallbackMaterial url="c4cA8UN.jpg" />}>
-        <VideoMaterial url="drei.mp4" />
-      </Suspense>
-    </mesh>
-  )
-}
+// function Scene() {
+//   const size = useAspect(1800, 1000)
+//   return (
+//     <mesh scale={size}>
+//       <planeGeometry />
+//       <Suspense fallback={<FallbackMaterial url="c4cA8UN.jpg" />}>
+//         <VideoMaterial url="drei.mp4" />
+//       </Suspense>
+//     </mesh>
+//   )
+// }
 
-function VideoMaterial({ url }) {
-  const texture = useVideoTexture(url)
-  return <meshBasicMaterial map={texture} toneMapped={false} />
-}
+// function VideoMaterial({ url }) {
+//   const texture = useVideoTexture(url)
+//   return <meshBasicMaterial map={texture} toneMapped={false} />
+// }
 
-function FallbackMaterial({ url }) {
-  const texture = useTexture(url)
-  return <meshBasicMaterial map={texture} toneMapped={false} />
-}
+// function FallbackMaterial({ url }) {
+//   const texture = useTexture(url)
+//   return <meshBasicMaterial map={texture} toneMapped={false} />
+// }
 
 
 
@@ -56,14 +56,14 @@ const InfoSection = ({
   lightText,
   headline,
   description,
-  buttonLabel,
+  // buttonLabel,
   img,
   alt,
   id,
   primary,
   darkText,
-  dark,
-  dark2
+  // dark,
+  // dark2
 }) => {
   console.log(primary);
   return (
