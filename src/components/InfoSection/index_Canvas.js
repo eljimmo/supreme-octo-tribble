b981510fd50ac7324cfe8d043d5e4c64ea753179
blackalien <jsmobst@gmail.com>
2023-04-29 00:18:13 -0500

    leibniz 3

--- src/components/InfoSection/index_Canvas.js
+++ src/components/InfoSection/index_Canvas.js
@@ -1,5 +1,4 @@
 import { React, Suspense } from 'react';
-// import { Button, HrefButton, Button1 } from '../ButtonElements';
 import {
   InfoContainer,
   InfoWrapper,
@@ -10,42 +9,9 @@ import {
   TopLine,
   Heading,
   Subtitle,
-  // BtnWrap,
-  // ImgWrap,
-  // Img
 } from './InfoElements';
 import { Canvas } from '@react-three/fiber'
-// import { useAspect, useVideoTexture, useTexture } from '@react-three/drei'
 import Model from '../GEO/Geo'
-// import SombreroSuperficieMath from '../3d_models/Sombrero_superficie_math';
-// import Scenemodel from '../3d_models/Scene_draco'
-// import { Button } from '@mantine/core';
-
-
-
-
-// function Scene() {
-//   const size = useAspect(1800, 1000)
-//   return (
-//     <mesh scale={size}>
-//       <planeGeometry />
-//       <Suspense fallback={<FallbackMaterial url="c4cA8UN.jpg" />}>
-//         <VideoMaterial url="drei.mp4" />
-//       </Suspense>
-//     </mesh>
-//   )
-// }
-
-// function VideoMaterial({ url }) {
-//   const texture = useVideoTexture(url)
-//   return <meshBasicMaterial map={texture} toneMapped={false} />
-// }
-
-// function FallbackMaterial({ url }) {
-//   const texture = useTexture(url)
-//   return <meshBasicMaterial map={texture} toneMapped={false} />
-// }
-
 
 
 
@@ -56,14 +22,10 @@ const InfoSectionCanva = ({
   lightText,
   headline,
   description,
-  buttonLabel,
-  img,
-  alt,
   id,
   primary,
   darkText,
-  dark,
-  dark2
+
 }) => {
   console.log(primary);
   return (
