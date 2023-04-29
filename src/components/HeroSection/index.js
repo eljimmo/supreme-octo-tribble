0637b61ea73267e9baea386cd3f0809e46179c54
blackalien <jsmobst@gmail.com>
2023-04-28 20:00:06 -0500

    leibniz 4

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,118 +1,118 @@
-import React, { useState, Suspense, useRef, useMemo } from 'react';
-// import Video from './videos/iStock_optic.mp4';
-import {
-  HeroContainer,
-  HeroBg,
-  HeroContent,
-} from './HeroElements';
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { Text, Cylinder } from '@react-three/drei'
-import * as THREE from 'three'
-import Typewriter from 'typewriter-effect';
+// import React, { useState, Suspense, useRef, useMemo } from 'react';
+// // import Video from './videos/iStock_optic.mp4';
+// import {
+//   HeroContainer,
+//   HeroBg,
+//   HeroContent,
+// } from './HeroElements';
+// import { Canvas, useFrame, useThree } from '@react-three/fiber'
+// import { Text, Cylinder } from '@react-three/drei'
+// import * as THREE from 'three'
+// import Typewriter from 'typewriter-effect';
 
 
 
-function Caption({ children }) {
-  const { width } = useThree((state) => state.viewport)
-  return (
-    <Text
-      position={[0, 0, 0]}
-      lineHeight={0.8}
-      fontSize={width / 8}
-      material-toneMapped={false}
-      anchorX="center"
-      anchorY="middle">
-      {children}
-    </Text>
-  )
-}
-
-
-
-
-
-
-export default function HeroSection() {
-
-  // const [hover, setHover] = useState(false);
-  // const onHover = () => {
-  //   setHover(!hover);
-  // };
-  return (
-    <HeroContainer id='home'>
-
-
-
-      <HeroBg>
-
-
-      <div style={{ color: 'white', position: [0, 0, 0] }}>
-
-<Typewriter
-options={{
-strings: ['Creating Intelligence', 'Creating Agency', 'Creating Consciousness'],
-autoStart: true,
-loop: true,
-}}
-/>
-
-</div>
-
-
-        {/* <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' /> */}
+// function Caption({ children }) {
+//   const { width } = useThree((state) => state.viewport)
+//   return (
+//     <Text
+//       position={[0, 0, 0]}
+//       lineHeight={0.8}
+//       fontSize={width / 8}
+//       material-toneMapped={false}
+//       anchorX="center"
+//       anchorY="middle">
+//       {children}
+//     </Text>
+//   )
+// }
 
 
 
 
-      </HeroBg>
+
+
+// export default function HeroSection() {
+
+//   // const [hover, setHover] = useState(false);
+//   // const onHover = () => {
+//   //   setHover(!hover);
+//   // };
+//   return (
+//     <HeroContainer id='home'>
 
 
 
-      <HeroContent>
+//       <HeroBg>
 
-        {/* <Canvas 
-        shadows
-    gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
-    camera={{ position: [3, 0, 2], fov: 53.5, near: 1, far: 10 }}
-    onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}>
-      <Experience />
-      </Canvas> */}
+
+//       <div style={{ color: 'white', position: [0, 0, 0] }}>
+
+// <Typewriter
+// options={{
+// strings: ['Creating Intelligence', 'Creating Agency', 'Creating Consciousness'],
+// autoStart: true,
+// loop: true,
+// }}
+// />
+
+// </div>
+
+
+//         {/* <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' /> */}
+
+
+
+
+//       </HeroBg>
+
+
+
+//       <HeroContent>
+
+//         {/* <Canvas 
+//         shadows
+//     gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
+//     camera={{ position: [3, 0, 2], fov: 53.5, near: 1, far: 10 }}
+//     onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}>
+//       <Experience />
+//       </Canvas> */}
 
      
-      {/* <Frosted_App/> */}
+//       {/* <Frosted_App/> */}
 
 
-      <Canvas
-        shadows
-    gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
-    camera={{ position: [3, 0, 2], fov: 53.5, near: 1, far: 40 }}
-    onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}>
+//       <Canvas
+//         shadows
+//     gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
+//     camera={{ position: [3, 0, 2], fov: 53.5, near: 1, far: 40 }}
+//     onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}>
 
-<Caption>LEIBNIZ</Caption>
-
-
-
-
-<ambientLight intensity={1} />
-<directionalLight position={[-2, 5, 2]} intensity={1} />
-<Suspense fallback={null}>
-
-
-</Suspense>
-</Canvas>
-
-</HeroContent>
+// <Caption>LEIBNIZ</Caption>
 
 
 
 
-   </HeroContainer>
+// <ambientLight intensity={1} />
+// <directionalLight position={[-2, 5, 2]} intensity={1} />
+// <Suspense fallback={null}>
+
+
+// </Suspense>
+// </Canvas>
+
+// </HeroContent>
 
 
 
 
-  );
-}
+//    </HeroContainer>
+
+
+
+
+//   );
+// }
 
 
 
