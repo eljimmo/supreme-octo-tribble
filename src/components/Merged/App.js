b3a453e038cf9d469334d75b72baf8ce801dc4b5
blackalien <jsmobst@gmail.com>
2023-04-28 22:46:48 -0500

    leibniz 777

--- src/components/Merged/App.js
+++ src/components/Merged/App.js
@@ -1,24 +1,24 @@
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { BakeShadows, ContactShadows, Environment, Html } from '@react-three/drei'
-import Model from './Model'
-import './styles.css'
+// import { Suspense } from 'react'
+// import { Canvas } from '@react-three/fiber'
+// import { BakeShadows, ContactShadows, Environment, Html } from '@react-three/drei'
+// import Model from './Model'
+// import './styles.css'
 
 
-export default function M_App() {
-  return (
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0.5, 1], fov: 50, near: 0.001 }} onCreated={(state) => (state.gl.shadowMap.autoUpdate = false)}>
-      <color attach="background" args={['#171717']} />
-      <ambientLight intensity={4} />
-      <spotLight position={[1, 5, 3]} angle={0.2} penumbra={1} intensity={3} castShadow shadow-mapSize={2048} />
-      <spotLight position={[0, 10, -10]} intensity={2} angle={0.04} penumbra={2} castShadow shadow-mapSize={1024} />
-      <Suspense fallback={<Html center className="loading" children="Loading..." />}>
-        <Model limit={50} position={[0, -0.0005, 0]} castShadow receiveShadow />
-        <ContactShadows frames={1} rotation-x={[Math.PI / 2]} position={[0, -0.4, 0]} far={1} width={1.5} height={1.5} blur={0.2} />
-        <Environment preset="night" />
-        <BakeShadows />
-      </Suspense>
-      {/* <CameraControls /> */}
-    </Canvas>
-  )
-}
+// export default function M_App() {
+//   return (
+//     <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0.5, 1], fov: 50, near: 0.001 }} onCreated={(state) => (state.gl.shadowMap.autoUpdate = false)}>
+//       <color attach="background" args={['#171717']} />
+//       <ambientLight intensity={4} />
+//       <spotLight position={[1, 5, 3]} angle={0.2} penumbra={1} intensity={3} castShadow shadow-mapSize={2048} />
+//       <spotLight position={[0, 10, -10]} intensity={2} angle={0.04} penumbra={2} castShadow shadow-mapSize={1024} />
+//       <Suspense fallback={<Html center className="loading" children="Loading..." />}>
+//         <Model limit={50} position={[0, -0.0005, 0]} castShadow receiveShadow />
+//         <ContactShadows frames={1} rotation-x={[Math.PI / 2]} position={[0, -0.4, 0]} far={1} width={1.5} height={1.5} blur={0.2} />
+//         <Environment preset="night" />
+//         <BakeShadows />
+//       </Suspense>
+//       {/* <CameraControls /> */}
+//     </Canvas>
+//   )
+// }
--- src/components/ScrollControl/src/Neuro_Evo.js
+++ src/components/ScrollControl/src/Neuro_Evo.js
@@ -1,182 +1,182 @@
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, createPortal, useFrame, useThree } from '@react-three/fiber'
-import { useFBO, useGLTF, useScroll, Text, Image, Scroll, Preload, ScrollControls, MeshTransmissionMaterial } from '@react-three/drei'
-import { easing } from 'maath'
-import RTApp from "..//../Router_Transit/App";
-import "./styles.css"
-import Navbar from '../../Navbar'
-import styled, { keyframes } from "styled-components";
-import { HeroContainer, HeroContainer400, HeroContainerCanva, HeroContainerTransparent } from '../../HeroSection/HeroElements'
+// import * as THREE from 'three'
+// import { useRef, useState } from 'react'
+// import { Canvas, createPortal, useFrame, useThree } from '@react-three/fiber'
+// import { useFBO, useGLTF, useScroll, Text, Image, Scroll, Preload, ScrollControls, MeshTransmissionMaterial } from '@react-three/drei'
+// import { easing } from 'maath'
+// import RTApp from "..//../Router_Transit/App";
+// import "./styles.css"
+// import Navbar from '../../Navbar'
+// import styled, { keyframes } from "styled-components";
+// import { HeroContainer, HeroContainer400, HeroContainerCanva, HeroContainerTransparent } from '../../HeroSection/HeroElements'
 
 
 
-const TText = styled.div`
-  font-size: var(--fontxs);
-  color: white;
-  width: 25%;
-  height: 60vh;
-  display: inline-block;
-  flex-direction: row-reverse;
-  justify-content: start;
-  span {
-    margin: 0.2rem 15px;
-    padding-left: 2rem;
-  }
-  @media screen and (max-width: 64em) {
-    width: 50%;
-  }
-  @media screen and (max-width: 48em) {
-    width: 100%;
-    font-size: var(--fontxxs);
-    span {
-      width: 40%;
-      padding-left: 1rem;
-    }
-    & > *:last-child {
-      align-self: flex-end;
-      padding-left: 10;
-      padding-right: 1rem;
-      text-align: right;
-    }
-  }
-`;
+// const TText = styled.div`
+//   font-size: var(--fontxs);
+//   color: white;
+//   width: 25%;
+//   height: 60vh;
+//   display: inline-block;
+//   flex-direction: row-reverse;
+//   justify-content: start;
+//   span {
+//     margin: 0.2rem 15px;
+//     padding-left: 2rem;
+//   }
+//   @media screen and (max-width: 64em) {
+//     width: 50%;
+//   }
+//   @media screen and (max-width: 48em) {
+//     width: 100%;
+//     font-size: var(--fontxxs);
+//     span {
+//       width: 40%;
+//       padding-left: 1rem;
+//     }
+//     & > *:last-child {
+//       align-self: flex-end;
+//       padding-left: 10;
+//       padding-right: 1rem;
+//       text-align: right;
+//     }
+//   }
+// `;
 
 
 
-export default function Neuro_scroll() {
-    // const [isOpen, setIsOpen] = useState(false);
+// export default function Neuro_scroll() {
+//     // const [isOpen, setIsOpen] = useState(false);
 
-    // const toggle = () => {
-    //   setIsOpen(!isOpen);
-    // };
-  return (
-    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', background: 'black' }}>
-              {/* <Navbar toggle={toggle} /> */}
+//     // const toggle = () => {
+//     //   setIsOpen(!isOpen);
+//     // };
+//   return (
+//     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', background: 'black' }}>
+//               {/* <Navbar toggle={toggle} /> */}
 
-    <Canvas camera={{ position: [0, 0, 20], fov: 15
-    // set the background color of the scene to black
-    , background: new THREE.Color(0x000000)
+//     <Canvas camera={{ position: [0, 0, 20], fov: 15
+//     // set the background color of the scene to black
+//     , background: new THREE.Color(0x000000)
     
     
-    }}>
-      <ScrollControls damping={0.2} pages={3} distance={0.5}>
-          <Scroll>
+//     }}>
+//       <ScrollControls damping={0.2} pages={3} distance={0.5}>
+//           <Scroll>
 
-            <Typography />
-            <Images />
+//             <Typography />
+//             <Images />
 
-          </Scroll>
+//           </Scroll>
 
-          {/** This is a helper that pre-emptively makes threejs aware of all geometries, textures etc
-               By default threejs will only process objects if they are "seen" by the camera leading 
-               to jank as you scroll down. With <Preload> that's solved.  */}
-          <Preload />
-
-
-
-
-          <Scroll html>
-
-
-            <div style={{ transform: 'translate3d(65vw, 192vh, 0)', color: 'white' }}>
+//           {/** This is a helper that pre-emptively makes threejs aware of all geometries, textures etc
+//                By default threejs will only process objects if they are "seen" by the camera leading 
+//                to jank as you scroll down. With <Preload> that's solved.  */}
+//           <Preload />
 
 
 
 
+//           <Scroll html>
 
 
-            structure are inspired by the human brain, mimicking the way biological neurons signal.
+//             <div style={{ transform: 'translate3d(65vw, 192vh, 0)', color: 'white' }}>
+
+
+
+
+
+
+//             structure are inspired by the human brain, mimicking the way biological neurons signal.
             
-                                      <br />
-                                      Learning can be supervised, semi-supervised or unsupervised
-                                        <br />
+//                                       <br />
+//                                       Learning can be supervised, semi-supervised or unsupervised
+//                                         <br />
 
 
-                                        inspired by information processing and distributed communication nodes
-                                                      <br />
+//                                         inspired by information processing and distributed communication nodes
+//                                                       <br />
 
 
-                                                      Function approximation, or regression analysis, including time series prediction.
-                                                      <br/>
+//                                                       Function approximation, or regression analysis, including time series prediction.
+//                                                       <br/>
 
-                                                      Classification, including pattern and sequence recognition, and sequential decision making.
-                                                      <br/>
+//                                                       Classification, including pattern and sequence recognition, and sequential decision making.
+//                                                       <br/>
 
-                                                      Data processing, including filtering, clustering, blind signal separation and compression.
-                                                      <br/>
-            </div>
-          </Scroll>
-
-
-
-      </ScrollControls>
-
-
-    </Canvas>
-
-    {/* <HeroContainerCanva>
-    heellloo3333
-
-    </HeroContainerCanva> */}
+//                                                       Data processing, including filtering, clustering, blind signal separation and compression.
+//                                                       <br/>
+//             </div>
+//           </Scroll>
 
 
 
+//       </ScrollControls>
+
+
+//     </Canvas>
+
+//     {/* <HeroContainerCanva>
+//     heellloo3333
+
+//     </HeroContainerCanva> */}
 
 
 
-    {/* <div style={{ width: '100vw', height: '500px', color: 'white' }}>
 
-    heellloo3333
-    </div>  */}
 
-    </div>
+
+//     {/* <div style={{ width: '100vw', height: '500px', color: 'white' }}>
+
+//     heellloo3333
+//     </div>  */}
+
+//     </div>
 
   
 
 
-  )
-}
+//   )
+// }
 
 
-function Images() {
-  const group = useRef()
-  const data = useScroll()
-  const { width, height } = useThree((state) => state.viewport)
-  useFrame(() => {
-    group.current.children[0].material.zoom = 1 + data.range(0, 1 / 3) / 3
-    group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3
-    group.current.children[2].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 2
-    group.current.children[3].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 2
-    group.current.children[4].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 2
-    group.current.children[5].material.grayscale = 1 - data.range(1.6 / 3, 1 / 3)
-    group.current.children[6].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
-  })
-  return (
-    <group ref={group}>
-      <Image position={[-2, 0, 0]} scale={[4, height, 1]} url="/invest.jpg" />
-      <Image position={[2, -1, 3]} scale={3} url="/nodes.jpg" />
-      <Image position={[-1.05, -height, 6]} scale={[1, 3, 1]} url="/crosshair.jpg" />
-      <Image position={[1.6, -height, 1.5, 7.5]} scale={[1, 2, 1]} url="/Server.jpg" />
-      <Image position={[0.75, -height, 10.5]} scale={1.5} url="/Blue.jpg" />
-      <Image position={[0, -height * 1.5, 5.5]} scale={[1.5, 3, 1]} url="/board.jpg" />
-      <Image position={[0, -height * 2 - height / 4, 0]} scale={[width, height / 1.1, 1]} url="/ComputingAbstract.jpg" />
-    </group>
-  )
-}
+// function Images() {
+//   const group = useRef()
+//   const data = useScroll()
+//   const { width, height } = useThree((state) => state.viewport)
+//   useFrame(() => {
+//     group.current.children[0].material.zoom = 1 + data.range(0, 1 / 3) / 3
+//     group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3
+//     group.current.children[2].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 2
+//     group.current.children[3].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 2
+//     group.current.children[4].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 2
+//     group.current.children[5].material.grayscale = 1 - data.range(1.6 / 3, 1 / 3)
+//     group.current.children[6].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
+//   })
+//   return (
+//     <group ref={group}>
+//       <Image position={[-2, 0, 0]} scale={[4, height, 1]} url="/invest.jpg" />
+//       <Image position={[2, -1, 3]} scale={3} url="/nodes.jpg" />
+//       <Image position={[-1.05, -height, 6]} scale={[1, 3, 1]} url="/crosshair.jpg" />
+//       <Image position={[1.6, -height, 1.5, 7.5]} scale={[1, 2, 1]} url="/Server.jpg" />
+//       <Image position={[0.75, -height, 10.5]} scale={1.5} url="/Blue.jpg" />
+//       <Image position={[0, -height * 1.5, 5.5]} scale={[1.5, 3, 1]} url="/board.jpg" />
+//       <Image position={[0, -height * 2 - height / 4, 0]} scale={[width, height / 1.1, 1]} url="/ComputingAbstract.jpg" />
+//     </group>
+//   )
+// }
 
-function Typography() {
-  const state = useThree()
-  const { width, height } = state.viewport.getCurrentViewport(state.cameta, [0, 0, 12])
-  const shared = { font: '/Inter-Regular.woff', letterSpacing: -0.1, color: 'white' }
-  return (
-    <>
-      {/* <Text children="Neuro evolution" anchorX="left" position={[-width / 2.5, -height / 1, 2]} {...shared} /> */}
-      <Text children="generic population-based " anchorX="right" position={[width / 1, -height * 2, 1]} {...shared} />
-      {/* <Text children="metaheuristic optimization algorithm" position={[0, -height * 4, 1]} {...shared} /> */}
-      <Text children="inspired by biological evolution" position={[0, -height * 1, 1]} {...shared} />
-      <Text children="Leibniz" position={[0, 0, 0]} {...shared} />
+// function Typography() {
+//   const state = useThree()
+//   const { width, height } = state.viewport.getCurrentViewport(state.cameta, [0, 0, 12])
+//   const shared = { font: '/Inter-Regular.woff', letterSpacing: -0.1, color: 'white' }
+//   return (
+//     <>
+//       {/* <Text children="Neuro evolution" anchorX="left" position={[-width / 2.5, -height / 1, 2]} {...shared} /> */}
+//       <Text children="generic population-based " anchorX="right" position={[width / 1, -height * 2, 1]} {...shared} />
+//       {/* <Text children="metaheuristic optimization algorithm" position={[0, -height * 4, 1]} {...shared} /> */}
+//       <Text children="inspired by biological evolution" position={[0, -height * 1, 1]} {...shared} />
+//       <Text children="Leibniz" position={[0, 0, 0]} {...shared} />
 
-    </>
-  )
-}
+//     </>
+//   )
+// }
