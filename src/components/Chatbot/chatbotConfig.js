2ac44e9930490579addcfdd1c9de1fbb3d791c94
blackalien <jsmobst@gmail.com>
2023-05-01 09:38:38 -0500

    chatbot none 2

--- src/components/Chatbot/chatbotConfig.js
+++ src/components/Chatbot/chatbotConfig.js
@@ -1,59 +1,59 @@
-import React from "react";
-import { createChatBotMessage } from "react-chatbot-kit";
+// import React from "react";
+// import { createChatBotMessage } from "react-chatbot-kit";
 
-import HighSchoolBranches from "./components/HighSchoolBranches";
-import List from "./components/List";
+// import HighSchoolBranches from "./components/HighSchoolBranches";
+// import List from "./components/List";
 
-const botName = "Guidance Bot";
+// const botName = "Guidance Bot";
 
-const config = {
-  botName: botName,
-  initialMessages: [
-    createChatBotMessage(
-      `Hi There! I am ${botName}. I am here to guide you through your journey here.`
-    ),
-    createChatBotMessage(
-      "May I know what brings you here today?",
-      {
-        withAvatar: true,
-        widget: "highSchoolBranches"
-      }
-    )
-  ],
-  customStyles: {
-    botMessageBox: {
-      backgroundColor: "#a1c3f0"
-    },
-    chatButton: {
-      backgroundColor: "#ccdbfd"
-    }
-  },
-  widgets: [
-    {
-      widgetName: "highSchoolBranches",
-      widgetFunc: (props) => <HighSchoolBranches {...props} />
-    },
-    {
-      widgetName: "careerLinks",
-      widgetFunc: (props) => <List {...props} />,
-      props: {
-        options: [
-          {
-            text: "Yes",
-            id: 1
-          },
-          {
-            text: "No",
-            id: 2
-          },
-          {
-            text: "May Be",
-            id: 3
-          }
-        ]
-      }
-    }
-  ]
-};
+// const config = {
+//   botName: botName,
+//   initialMessages: [
+//     createChatBotMessage(
+//       `Hi There! I am ${botName}. I am here to guide you through your journey here.`
+//     ),
+//     createChatBotMessage(
+//       "May I know what brings you here today?",
+//       {
+//         withAvatar: true,
+//         widget: "highSchoolBranches"
+//       }
+//     )
+//   ],
+//   customStyles: {
+//     botMessageBox: {
+//       backgroundColor: "#a1c3f0"
+//     },
+//     chatButton: {
+//       backgroundColor: "#ccdbfd"
+//     }
+//   },
+//   widgets: [
+//     {
+//       widgetName: "highSchoolBranches",
+//       widgetFunc: (props) => <HighSchoolBranches {...props} />
+//     },
+//     {
+//       widgetName: "careerLinks",
+//       widgetFunc: (props) => <List {...props} />,
+//       props: {
+//         options: [
+//           {
+//             text: "Yes",
+//             id: 1
+//           },
+//           {
+//             text: "No",
+//             id: 2
+//           },
+//           {
+//             text: "May Be",
+//             id: 3
+//           }
+//         ]
+//       }
+//     }
+//   ]
+// };
 
-export default config;
+// export default config;
