f041c5f19b7dd45425c953985e46c29ebc0dd060
blackalien <jsmobst@gmail.com>
2023-05-01 17:26:55 -0500

    cleaner ai pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import HomePage from './pages/Home_page_index';
+import AIPage from './pages/AI_Models';
 
 
 export default function App() {
@@ -14,6 +15,21 @@ export default function App() {
             <Route path="/" element={<HomePage />} />
 
           
+
+            
+
+            {/* <Route path="/neuroevolution" element={<Welcome />} /> */}
+
+            {/* <Route path="/MachineLearning" element={<Machine_page />} /> */}
+
+            <Route path="/AImodels" element={<AIPage />} />
+
+            {/* <Route path="/About" element={<AboutIndex />} /> */}
+
+            {/* <Route path="/s" element={<StockSearch />} /> */}
+
+            {/* <Route path="/Demo" element={<Dashboard />} /> */}
+
           
           </Routes>
 
--- src/pages/AI_Models.js
+++ src/pages/AI_Models.js
@@ -1,53 +1,32 @@
-// import React, { useState } from 'react';
-// import InfoSection from '../components/InfoSection';
-
-// import {
-//   homeObjTwo,
-//   homeObjThree,
-
-// } from '../components/InfoSection/Data';
-// import InfoSectionCanva from '../components/InfoSection/index_Canvas';
-// import { Affix, Button, rem } from '@mantine/core';
-// import Navbar from '../components/Navbar';
-// import Footer from '../components/Footer/index';
-// import Sidebar from '../components/Sidebar/index';
-// import { HeroContainerTransparent } from '../components/HeroSection/HeroElements'; 
-// import Services from '../components/Services';
-// import KamdoApp from '../components/Kamdo/index';
-// // import SeriesSimulationsModeling from '../views/StockSearch/SSCompos/Tabs/Analytic_Dash/Time_Series';
+import React, { useState } from 'react';
+import { Affix, Button, rem } from '@mantine/core';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer/index';
+import Sidebar from '../components/Sidebar/index';
 
 
 
-// export default function AIPage() {
-//   const [isOpen, setIsOpen] = useState(false);
+export default function AIPage() {
+  const [isOpen, setIsOpen] = useState(false);
 
-//   const toggle = () => {
-//     setIsOpen(!isOpen);
-//   };
-//   return (
-//     <>
-//       <Navbar toggle={toggle} />
-//       <Sidebar isOpen={isOpen} toggle={toggle} />
-//       <HeroContainerTransparent>
-//       <KamdoApp/>
-// </HeroContainerTransparent>
-//   <InfoSectionCanva {...homeObjTwo} />
-// <InfoSection {...homeObjThree} /> 
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
 
-// {/* <SeriesSimulationsModeling/> */}
+      <Affix position={{ bottom: rem(20), right: rem(20) }}>
+            <Button
+              variant="outline"
+            >
+              I am still in Development.
+            </Button>
+      </Affix>
 
-//       <Affix position={{ bottom: rem(20), right: rem(20) }}>
-//             <Button
-//               variant="outline"
-//             >
-//               I am still in Development.
-//             </Button>
-//       </Affix>
-
-//       <Services/>
-
-//         <Footer/>
-//     </>
-//   );
-// }
+        <Footer/>
+    </>
+  );
+}
 
