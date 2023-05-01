7c58780de653c6e1b9007edfdc425bb35edf5320
blackalien <jsmobst@gmail.com>
2023-04-30 21:34:16 -0500

    chatbot ai no time series 2

--- src/pages/AI_Models.js
+++ src/pages/AI_Models.js
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
-import InfoSection from '../components/InfoSection';
+// import React, { useState } from 'react';
+// import InfoSection from '../components/InfoSection';
 
-import {
-  homeObjTwo,
-  homeObjThree,
+// import {
+//   homeObjTwo,
+//   homeObjThree,
 
-} from '../components/InfoSection/Data';
-import InfoSectionCanva from '../components/InfoSection/index_Canvas';
-import { Affix, Button, rem } from '@mantine/core';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer/index';
-import Sidebar from '../components/Sidebar/index';
-import { HeroContainerTransparent } from '../components/HeroSection/HeroElements'; 
-import Services from '../components/Services';
-import KamdoApp from '../components/Kamdo/index';
-// import SeriesSimulationsModeling from '../views/StockSearch/SSCompos/Tabs/Analytic_Dash/Time_Series';
+// } from '../components/InfoSection/Data';
+// import InfoSectionCanva from '../components/InfoSection/index_Canvas';
+// import { Affix, Button, rem } from '@mantine/core';
+// import Navbar from '../components/Navbar';
+// import Footer from '../components/Footer/index';
+// import Sidebar from '../components/Sidebar/index';
+// import { HeroContainerTransparent } from '../components/HeroSection/HeroElements'; 
+// import Services from '../components/Services';
+// import KamdoApp from '../components/Kamdo/index';
+// // import SeriesSimulationsModeling from '../views/StockSearch/SSCompos/Tabs/Analytic_Dash/Time_Series';
 
 
 
-export default function AIPage() {
-  const [isOpen, setIsOpen] = useState(false);
+// export default function AIPage() {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Navbar toggle={toggle} />
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <HeroContainerTransparent>
-      <KamdoApp/>
-</HeroContainerTransparent>
-  <InfoSectionCanva {...homeObjTwo} />
-<InfoSection {...homeObjThree} /> 
+//   const toggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   return (
+//     <>
+//       <Navbar toggle={toggle} />
+//       <Sidebar isOpen={isOpen} toggle={toggle} />
+//       <HeroContainerTransparent>
+//       <KamdoApp/>
+// </HeroContainerTransparent>
+//   <InfoSectionCanva {...homeObjTwo} />
+// <InfoSection {...homeObjThree} /> 
 
-{/* <SeriesSimulationsModeling/> */}
+// {/* <SeriesSimulationsModeling/> */}
 
-      <Affix position={{ bottom: rem(20), right: rem(20) }}>
-            <Button
-              variant="outline"
-            >
-              I am still in Development.
-            </Button>
-      </Affix>
+//       <Affix position={{ bottom: rem(20), right: rem(20) }}>
+//             <Button
+//               variant="outline"
+//             >
+//               I am still in Development.
+//             </Button>
+//       </Affix>
 
-      <Services/>
+//       <Services/>
 
-        <Footer/>
-    </>
-  );
-}
+//         <Footer/>
+//     </>
+//   );
+// }
 
