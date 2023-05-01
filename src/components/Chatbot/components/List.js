febcdfc5e4e2fc6bc7d9a96b6cfd9374c93880de
blackalien <jsmobst@gmail.com>
2023-05-01 10:21:06 -0500

    chatbot added

--- src/components/Chatbot/components/List.js
+++ src/components/Chatbot/components/List.js
@@ -1,16 +1,16 @@
-// import React from "react";
+import React from "react";
 
-// import "./List.css";
+import "./List.css";
 
-// const List = (props) => {
-//   console.log(props);
-//   const linkMarkup = props.options.map((link) => (
-//     <li key={link.id} className="link-list-item">
-//       {link.text}
-//     </li>
-//   ));
+const List = (props) => {
+  console.log(props);
+  const linkMarkup = props.options.map((link) => (
+    <li key={link.id} className="link-list-item">
+      {link.text}
+    </li>
+  ));
 
-//   return <ul className="link-list">{linkMarkup}</ul>;
-// };
+  return <ul className="link-list">{linkMarkup}</ul>;
+};
 
-// export default List;
+export default List;
