9d1b0d084cbd15a4dd79b8e021887ffce7de1d3a
blackalien <jsmobst@gmail.com>
2023-05-01 17:59:37 -0500

    cleaner ai pages 222

--- src/App.js
+++ src/App.js
@@ -1,41 +1,100 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import React from "react";
 import HomePage from './pages/Home_page_index';
 import AIPage from './pages/AI_Models';
 
 
-export default function App() {
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink
+} from 'react-router-dom';
+
+
+function App() {
   return (
-    <div>
-
-
-      <BrowserRouter>
-          <Routes>
-            
-            <Route path="/" element={<HomePage />} />
-
-          
-
-            
-
-            {/* <Route path="/neuroevolution" element={<Welcome />} /> */}
-
-            {/* <Route path="/MachineLearning" element={<Machine_page />} /> */}
-
-            <Route path="/AImodels" element={<AIPage />} />
-
-            {/* <Route path="/About" element={<AboutIndex />} /> */}
-
-            {/* <Route path="/s" element={<StockSearch />} /> */}
-
-            {/* <Route path="/Demo" element={<Dashboard />} /> */}
-
-          
-          </Routes>
-
-        </BrowserRouter>
-
-    </div>
+<div className="App">
+<Router>
+    {/* <Navbar /> */}
+    <Routes>
+    <Route exact path='/' element={<HomePage />} />
+    <Route path='/AImodels' element={<AIPage />} />
+    {/* <Route path='/Login' element={<Login />} /> */}
+    </Routes>
+</Router>
+</div>
+   
   );
 }
+
+export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <nav>
+//           <ul>
+//             <li><NavLink exact to="/" activeClassName="selected">Home</NavLink></li>
+//             <li><NavLink to="/AImodels" activeClassName="selected">Contact</NavLink></li>
+//           </ul>
+//         </nav>
+//         <Routes>
+//           <Route path="/AImodels">
+//             <AIPage />
+//           </Route>
+//           <Route path="/" element={<HomePage />} /> 
+//           <Route path="/AImodels" element={<AIPage />} />
+
+
+//             {/* <HomePage /> */}
+//           {/* </Route> */}
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+// export default function App() {
+//   return (
+//     <div>
+
+
+//       <BrowserRouter>
+//           <Routes>
+            
+//             <Route path="/" element={<HomePage />} />
+
+          
+
+            
+
+//             {/* <Route path="/neuroevolution" element={<Welcome />} /> */}
+
+//             {/* <Route path="/MachineLearning" element={<Machine_page />} /> */}
+
+//             <Route path="/AImodels" element={<AIPage />} />
+
+//             {/* <Route path="/About" element={<AboutIndex />} /> */}
+
+//             {/* <Route path="/s" element={<StockSearch />} /> */}
+
+//             {/* <Route path="/Demo" element={<Dashboard />} /> */}
+
+          
+//           </Routes>
+
+//         </BrowserRouter>
+
+//     </div>
+//   );
+// }
 
--- src/pages/AI_Models.js
+++ src/pages/AI_Models.js
@@ -1,32 +1,46 @@
-import React, { useState } from 'react';
-import { Affix, Button, rem } from '@mantine/core';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer/index';
-import Sidebar from '../components/Sidebar/index';
+import React, { Component } from 'react';
+// import { Affix, Button, rem } from '@mantine/core';
+// import Navbar from '../components/Navbar';
+// import Footer from '../components/Footer/index';
+// import Sidebar from '../components/Sidebar/index';
 
 
 
-export default function AIPage() {
-  const [isOpen, setIsOpen] = useState(false);
+// export default function AIPage() {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Navbar toggle={toggle} />
-      <Sidebar isOpen={isOpen} toggle={toggle} />
+//   const toggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   return (
+//     <>
+//       <Navbar toggle={toggle} />
+//       <Sidebar isOpen={isOpen} toggle={toggle} />
 
-      <Affix position={{ bottom: rem(20), right: rem(20) }}>
-            <Button
-              variant="outline"
-            >
-              I am still in Development.
-            </Button>
-      </Affix>
+//       <Affix position={{ bottom: rem(20), right: rem(20) }}>
+//             <Button
+//               variant="outline"
+//             >
+//               I am still in Development.
+//             </Button>
+//       </Affix>
 
-        <Footer/>
-    </>
-  );
+//         <Footer/>
+//     </>
+//   );
+// }
+
+
+class AIPage extends Component {
+
+  render() 
+  
+  {
+    return (
+      <div className="Contact">
+        This is the contact page.
+      </div>
+    )
+  }
 }
-
+export default AIPage;
