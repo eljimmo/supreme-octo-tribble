32298db9cef8be25ed37864509487024994d0c19
blackalien <jsmobst@gmail.com>
2023-05-02 17:14:43 -0500

    chats

--- src/components/Chatbot/App.js
+++ src/components/Chatbot/App.js
@@ -1,42 +1,67 @@
-import React, { Component } from "react";
-import "./styles.css";
+import React, { useState } from "react";
 import Chatbot from "react-chatbot-kit";
-import config from "./chatbotConfig";
-import ActionProvider from "./ActionProvider";
-import MessageParser from "./MessageParser";
-import { ActionIcon } from '@mantine/core';
-import { IconRobot } from '@tabler/icons-react';
+import { ConditionallyRender } from "react-util-kit";
 
-class ChatApp extends Component {
-  state = {
-    showBot: false
-  };
 
-  handleShowBot = () => {
-    this.setState({
-      showBot: !this.state.showBot
-    });
-  };
+import { ReactComponent as Logo } from "./assets/icons/logo.svg";
+import { ReactComponent as ButtonIcon } from "./assets/icons/robot.svg";
 
-  render() {
-    return (
-      <div className="app">
-        {this.state.showBot && (
-          <Chatbot
-            config={config}
-            actionProvider={ActionProvider}
-            messageParser={MessageParser}
-            className="app_Chatbot"
+import config from "./bots/docsbot/config";
+import MessageParser from "./bots/docsbot/MessageParser";
+import ActionProvider from "./bots/docsbot/ActionProvider";
+
+import "./App.css";
+
+function ChatApp() {
+  const [showChatbot, toggleChatbot] = useState(true);
+
+  return (
+    <div className="App">
+
+        <Logo style={{ paddingTop: "40px", height: "150px", width: "150px" }} />
+        <h1 className="app-header">Project Leibniz Analytica</h1>
+        <div className="app-chatbot-container">
+          <ConditionallyRender
+            ifTrue={showChatbot}
+            show={
+              <Chatbot
+                config={config}
+                messageParser={MessageParser}
+                actionProvider={ActionProvider}
+              />
+            }
           />
-        )}
+        </div>
 
+        <button
+          className="app-chatbot-button"
+          onClick={() => toggleChatbot((prev) => !prev)}
+        >
+          <ButtonIcon className="app-chatbot-button-icon" />
+        </button>
 
-<ActionIcon>
-      <IconRobot size="3.125rem" onClick={this.handleShowBot}/>
-    </ActionIcon>
-      </div>
-    );
-  }
+        <div className="app-overview">
+          <h2 className="app-header">Overview</h2>
+          <p className="app-paragraph">
+            The AI bots work unnder the parameters given by the creator and with the data choosen. Utilizing the data and the parameters, the AI bots will be able to make predictions and give suggestions. 
+          </p>
+          <div className="app-overview-gist-container">
+          </div>
+
+          <h2 className="app-header">Contact Creator</h2>
+
+          <a
+            href="https://github.com/eljimmo"
+            className="app-quickstart-button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </div>
+
+    </div>
+  );
 }
 
 export default ChatApp;
