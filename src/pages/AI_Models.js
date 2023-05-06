d10e967e0502672baeb5f66c4a0dd6e612aaa421
blackalien <jsmobst@gmail.com>
2023-05-05 20:47:54 -0500

    config removed 2

--- src/pages/AI_Models.js
+++ src/pages/AI_Models.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Affix, Button, rem } from '@mantine/core';
 import Footer from '../components/Footer/index';
-
+import InfoSection from '../components/InfoSection';
+import InfoSectionCanva from '../components/InfoSection/index_Canvas';
+import {
+  homeObjOne,
+  // homeObjFive,
+  // homeObjFour,
+  homeObjThree,
+} from '../components/InfoSection/Data';
+import HeroSection from '../components/HeroSection';
 
 
 export default function AIPage() {
 
   return (
     <>
+<HeroSection/>
+<InfoSectionCanva {...homeObjOne} />
+<InfoSection {...homeObjThree} />
+
       <Affix position={{ bottom: rem(20), right: rem(20) }}>
             <Button
               variant="outline"
