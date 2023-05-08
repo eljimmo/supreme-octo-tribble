7c0a8539d07a7ece20a87561955706186abd4b59
blackalien <jsmobst@gmail.com>
2023-05-07 23:29:14 -0500

    maaaas babe 222

--- src/components/Services/index2.js
+++ src/components/Services/index2.js
@@ -7,23 +7,36 @@ import {
   ServicesH2,
   ServicesP
 } from './ServicesElements';
+import Typewriter from 'typewriter-effect';
 
 const Services = () => {
   return (
     <ServicesContainer id='services'>
       <ServicesH1>Artificial Intelligence</ServicesH1>
+
       <ServicesWrapper>
         <ServicesCard>
           <ServicesH2>
             AI Chatbots
           </ServicesH2>
+
+
+
           <ServicesP>
 Utilizing data provided by IEXCloud, Alpha Vantage, & Polygon API, we are able to create AI chatbots that can give you predictions and suggestions on what to do with your stocks.
 Create your own bots and change the way the internal model for the AI works.
           </ServicesP>
         </ServicesCard>
         <ServicesCard>
-          <ServicesH2>Sequential machine learning model</ServicesH2>
+          {/* <ServicesH2>Sequential machine learning model</ServicesH2> */}
+          <Typewriter
+  options={{
+    strings: ['Sequential machine learning model', 'Bidirectional RNN encoder and decoder', 'DNN(Deep Neural Networks)'],
+    autoStart: true,
+    loop: true,
+  }}
+/>
+
           <ServicesP>
 Create & Alter the parmeters of the model to see how it affects the predictions.
 
