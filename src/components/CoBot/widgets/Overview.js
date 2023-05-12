f9685c1b44a07cf073f1ee9fabc802e7b56dd04a
blackalien <jsmobst@gmail.com>
2023-05-11 21:57:43 -0500

    better agent

--- src/components/CoBot/widgets/Overview.js
+++ src/components/CoBot/widgets/Overview.js
@@ -2,16 +2,16 @@ import Options from "./Options";
 
 const GeneralOptions = (props) => {
   const options = [
-    // {
-    //   name: "Show global statistics",
-    //   handler: props.actionProvider.handleGlobalStats,
-    //   id: 1
-    // },
-    // {
-    //   name: "Show local statistics",
-    //   handler: props.actionProvider.handleLocalStats,
-    //   id: 2
-    // },
+    {
+      name: "Show me recommended stocks!",
+      handler: props.actionProvider.handleGlobalStats,
+      id: 1
+    },
+    {
+      name: "Show me what stocks to avoid!",
+      handler: props.actionProvider.handleLocalStats,
+      id: 2
+    },
     {
       name: "Creator contact",
       handler: props.actionProvider.handleContact,
