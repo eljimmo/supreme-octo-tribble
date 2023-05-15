7e3fc72992747c6a7c121131db6499490bc25ea6
blackalien <jsmobst@gmail.com>
2023-05-15 16:55:58 -0500

    crypto currency33

--- src/components/FrostedGlass/Overlay.js
+++ src/components/FrostedGlass/Overlay.js
@@ -36,21 +36,13 @@ export function Overlay() {
     <>
       <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
         <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>
-        {/* Project Leibniz Analytica */}
           <br />
-          {/* <Typewriter
-options={{
-strings: ['Creating Intelligence', 'Creating Agency', 'Creating Consciousness', 'Creating Solutions'],
-autoStart: true,
-loop: true,
-}}
-/>         */}
 </a>
         <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>
 
         <Typewriter
 options={{
-strings: ['The man who loves walking will walk further than the man who loves the destination.', 'The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela', 'Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson', 'It is during our darkest moments that we must focus to see the light. -Aristotle', 'Whoever is happy will make others happy too. -Anne Frank', 'You will face many defeats in life, but never let yourself be defeated. -Maya Angelou', 'In the end, it is not the years in your life that count. Its the life in your years. -Abraham Lincoln', 'Life is either a daring adventure or nothing at all. -Helen Keller', 'Many of lifes failures are people who did not realize how close they were to success when they gave up. -Thomas A. Edison'],
+strings: ['The man who loves walking will walk further than the man who loves the destination.', 'The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela', 'Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson', 'It is during our darkest moments that we must focus to see the light. -Aristotle', 'Whoever is happy will make others happy too. -Anne Frank', 'You will face many defeats in life, but never let yourself be defeated. -Maya Angelou', 'In the end, it is not the years in your life that count. Its the life in your years. -Abraham Lincoln', 'Life is either a daring adventure or nothing at all. -Helen Keller', 'Many of lifes failures are people who did not realize how close they were to success when they gave up. -Thomas A. Edison', 'All men dream: but not equally. Those who dream by night in the dusty recesses of their minds wake up in the day to find it was vanity, but the dreamers of the day are dangerous men, for they may act their dreams with open eyes, to make it possible.- T.E Lawrence', 'Your success will be proportioned to the amount of mental effort you devote to it. - T.E Lawrence '],
 autoStart: true,
 loop: true,
 }}
