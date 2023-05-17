cc0303934917b99cfb3cb04125018227136fb2c4
blackalien <jsmobst@gmail.com>
2023-05-17 15:48:30 -0500

    better 2

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Footer from '../components/Footer/index';
 import Testimonial from "../components/portfolio_react/components/testimonial/testimonial.component";
-// import Header from "../components/portfolio_react/components/header/header.component";
-// import Hero from "../components/portfolio_react/components/hero/hero.component";
-// import About from "../components/portfolio_react/components/about/about.component";
-// import Projects from "../components/portfolio_react/components/projects/projects.component"; 
-// import Header from '../components/portfolio_react/components/header/header.component';
 import Hero from '../components/portfolio_react/components/hero/hero.component';
 import About from '../components/portfolio_react/components/about/about.component';
 import '../components/portfolio_react/styles.css';
@@ -14,11 +9,6 @@ import '../components/portfolio_react/styles.css';
 export default function AboutIndex() {
     return (
       <>
-          {/* <Header/>
-          <Hero/>
-          <About/>
-          <Projects/> */}
-          {/* <Projects/> */}
           <Hero/>
           <About/>
 
