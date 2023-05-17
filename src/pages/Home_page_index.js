e64007ef02de9fad6a032db04446e0b3a20eeebe
blackalien <jsmobst@gmail.com>
2023-05-17 17:31:58 -0500

    navbar

--- src/pages/Home_page_index.js
+++ src/pages/Home_page_index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InfoSection from '../components/InfoSection';
 import { homeObjOne } from '../components/InfoSection/Data';
 import { Affix, rem, Button } from '@mantine/core';
@@ -6,12 +6,21 @@ import Footer from '../components/Footer/index';
 import { HeroContainerTransparent } from '../components/HeroSection/HeroElements';
 import FrostedApp from '../components/FrostedGlass/index';
 // import CApp from '../components/Chatbot/index';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar/index';
 
 
 
 export default function WelcomePage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
+          <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
       <HeroContainerTransparent>
       <FrostedApp/>
 </HeroContainerTransparent>
