96523d82b8a45ecb94ed619b347ea47f0da21096
blackalien <jsmobst@gmail.com>
2023-05-17 12:54:20 -0500

    chat rv leibniz

--- src/pages/Home_page_index.js
+++ src/pages/Home_page_index.js
@@ -3,14 +3,9 @@ import InfoSection from '../components/InfoSection';
 import { homeObjOne } from '../components/InfoSection/Data';
 import { Affix, rem, Button } from '@mantine/core';
 import Footer from '../components/Footer/index';
-// import ScApp from '../components/ScrollControl/src/App';
 import { HeroContainerTransparent } from '../components/HeroSection/HeroElements';
 import FrostedApp from '../components/FrostedGlass/index';
-// import StockSearch from '../views/StockSearch/Stock_Search_Index';
-// import Services from '../components/Services/index2';
-import CApp from '../components/CoBot/App';
-// import DashboardContent from '../components/Stock_Portfolio/Market_News';
-// import ACpp from '../components/Stock_Portfolio/Coin_Load';
+
 
 
 export default function HomePage() {
@@ -20,14 +15,9 @@ export default function HomePage() {
       <FrostedApp/>
 </HeroContainerTransparent>
 <InfoSection {...homeObjOne} />
-{/* <StockSearch/> */}
-{/* <ACpp/> */}
-{/* <Services/> */}
-{/* <DashboardContent/> */}
-{/* <ScApp/> */}
-      <Affix position={{ bottom: rem(20), left: rem(20) }}>
+      {/* <Affix position={{ bottom: rem(20), left: rem(20) }}>
             <CApp/>
-      </Affix>
+      </Affix> */}
       <Affix position={{ bottom: rem(20), right: rem(20) }}>
             <Button
               variant="outline"
