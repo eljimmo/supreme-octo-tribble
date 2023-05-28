bae9204dd10f7b8ff0af1c8fd304de9da71ddb00
blackalien <jsmobst@gmail.com>
2023-05-28 12:43:34 -0500

    leibniz center 3

--- src/components/MachineLearning/src/components/experience/Expreience.jsx
+++ src/components/MachineLearning/src/components/experience/Expreience.jsx
@@ -1,6 +1,21 @@
 import { Component } from "react";
 import { BsPatchCheckFill } from "react-icons/bs";
 import "./experience.css";
+import {
+  // InfoContainer,
+  // InfoWrapper,
+  // InfoWrapper2,
+  // InfoRow,
+  // Column1,
+  // Column2,
+  // TextWrapper,
+  // TextWrapper2,
+  TopLine,
+  // Heading1,
+  // Heading2,
+  // Subtitle,
+} from "../experience/elements";
+
 class Experience extends Component {
   render() {
     return (
@@ -8,7 +23,7 @@ class Experience extends Component {
 
         <div className="container experience__container">
           <div className="experience__frontend">
-            <h3>Frontend Development</h3>
+            <TopLine>Machine learning Algorithm Development</TopLine>
             <div className="experience__content">
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
@@ -55,48 +70,48 @@ class Experience extends Component {
             </div>
           </div>
           <div className="experience__backend">
-            <h3>Backend Development</h3>
+            <TopLine>Foundational Development of Chatbots and Virtual Assistants</TopLine>
             <div className="experience__content">
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
                 <div>
-                  <h4>NodeJs</h4>
-                  <small className="text-light">Experienced</small>
+                  <h4>Input Sequences</h4>
+                  <small className="text-light">Tokens</small>
                 </div>
               </article>
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
                 <div>
-                  <h4>Express</h4>
-                  <small className="text-light">Advandced</small>
+                  <h4>Embeddings</h4>
+                  <small className="text-light">Matrix of Vectors</small>
                 </div>
               </article>
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
                 <div>
-                  <h4>Mongo</h4>
-                  <small className="text-light">LoveIt</small>
+                  <h4>Self Attention Mechanism</h4>
+                  <small className="text-light">Attention Computation</small>
                 </div>
               </article>
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
                 <div>
-                  <h4>Next.js</h4>
-                  <small className="text-light">Intermediate</small>
+                  <h4>Multi-Head Self-Attention</h4>
+                  <small className="text-light">Transformer Architecture</small>
                 </div>
               </article>
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
                 <div>
-                  <h4>MySql</h4>
-                  <small className="text-light">Begginer</small>
+                  <h4>Feedforward Network</h4>
+                  <small className="text-light">Connected Layers and Activation Functions</small>
                 </div>
               </article>
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
                 <div>
-                  <h4>PHP</h4>
-                  <small className="text-light">Senior</small>
+                  <h4>Layer Normalization</h4>
+                  <small className="text-light"> stabilize the training process and prevent the model from overfitting</small>
                 </div>
               </article>
             </div>
