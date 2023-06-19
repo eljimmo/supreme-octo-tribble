2d35b1167256e66ead2d29fe002181a533807486
blackalien <jsmobst@gmail.com>
2023-06-19 17:09:08 -0500

    port 5000 local

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const app = express(); // create express app
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
+const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 // start express server on port 5000
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("server started on port 5000");
 });
