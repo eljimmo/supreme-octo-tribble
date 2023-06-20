ea5d5baaac06c0f9e12a04c1d1cf2d0840604119
blackalien <jsmobst@gmail.com>
2023-06-20 10:47:07 -0500

    PORT & HOST ADDED

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 // const PORT = process.env.PORT || 5001
+const host = '0.0.0.0';
 
 
 app.use((req, res, next) => {
@@ -16,6 +17,6 @@ app.use((req, res, next) => {
 });
 
 // start express server on port 5000
-app.listen(PORT, () => {
+app.listen(PORT, host, () => {
   console.log('Server is running on port: ' + PORT);
 });
