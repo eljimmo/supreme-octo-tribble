8fd22a7e20b4470c9f92807ed71fcff1210b3919
blackalien <jsmobst@gmail.com>
2023-06-19 19:46:19 -0500

    worker 222

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const app = express(); // create express app
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
+
 app.use(express.static("public"));
+
 const port = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5001
+
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
@@ -13,5 +17,5 @@ app.use((req, res, next) => {
 
 // start express server on port 5000
 app.listen(port, () => {
-  console.log("server started on port 5000");
+  console.log('Server is running on port: ' + port);
 });
