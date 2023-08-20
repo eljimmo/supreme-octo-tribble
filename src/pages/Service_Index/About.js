663caca7cc336760cd8c62b556d2d0f25d04ed14
blackalien <jsmobst@gmail.com>
2023-08-20 18:39:10 -0500

    another page added to about

--- src/pages/Service_Index/About.js
+++ src/pages/Service_Index/About.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import Footer from '../../components/Footer/index';
-import Hero from '../../components/portfolio_react/components/hero/hero.component';
-// import '../components/portfolio_react/styles.css';
 import '../../components/portfolio_react/styles.css';
 import Videoapp from '../../components/VideoTexture/App';
-import InfoSectionCanva from '../../components/InfoSection/index_Canvas';
-import {
-  homeObjFour
-} from '../../components/InfoSection/Data';
-// import Heroartificialintelligence from "../components/portfolio_react/components/hero/hero.artificialintelligence";
-// import Artificialintelligenceabout from "../components/portfolio_react/components/about/about.artificialintelligence";
-// import Experience from "../components/MachineLearning/src/components/experience/Expreience";
 import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/Sidebar/index';
-// import ScApp from '../components/ScrollControl/src/App';
+import App from '../../components/Landing/App';
+import Underlay from '../../components/Landing/Underlay';
+
+import {
+  HeroContainer,
+  HeroContainer252525
+} from "../../components/HeroSection/HeroElements";
 
 export default function AboutIndex() {
   const [isOpen, setIsOpen] = useState(false);
@@ -26,13 +23,10 @@ export default function AboutIndex() {
           <Navbar toggle={toggle} />
           <Sidebar isOpen={isOpen} toggle={toggle} />
           <Videoapp/>
-
-          {/* <ScApp/> */}
-          {/* <Hero/> */}
-          <InfoSectionCanva {...homeObjFour} />
-          {/* <Heroartificialintelligence/> */}
-          {/* <Artificialintelligenceabout/> */}
-          {/* <Experience/> */}
+          <HeroContainer252525>
+          <App/>
+          <Underlay/>
+            </HeroContainer252525>
           <Footer/>
       </>
     );
