d6796186bcccfacc3500bd2ffdad0187c3854b74
blackalien <jsmobst@gmail.com>
2023-08-27 17:32:52 -0500

    styling 24

--- src/components/TimeClock/Index.js
+++ src/components/TimeClock/Index.js
@@ -1,134 +1,121 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import moment from 'moment';
 import './styles.css';
 
 class Countdown extends React.Component {
-	state = {
-		days: undefined,
-		hours: undefined,
-		minutes: undefined,
-		seconds: undefined
-	}
-	
-	componentDidMount() {
-		this.interval = setInterval(() => {
-			const { timeTillDate, timeFormat } = this.props;
-			const then = moment(timeTillDate, timeFormat);
-			const now = moment();
-			const countdown = moment(then - now);
-			const days = countdown.format('D');
-			const hours = countdown.format('HH');
-			const minutes = countdown.format('mm');
-			const seconds = countdown.format('ss');
+  state = {
+    days: undefined,
+    hours: undefined,
+    minutes: undefined,
+    seconds: undefined
+  };
 
-			this.setState({ days, hours, minutes, seconds });
-		}, 1000);
-	}
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const { timeTillDate, timeFormat } = this.props;
+      const then = moment(timeTillDate, timeFormat);
+      const now = moment();
+      const countdown = moment(then - now);
+      const days = countdown.format('D');
+      const hours = countdown.format('HH');
+      const minutes = countdown.format('mm');
+      const seconds = countdown.format('ss');
 
-	componentWillUnmount() {
-		if(this.interval) {
-			clearInterval(this.interval);
-		}
-	}
-	
-	render() {
-		const { days, hours, minutes, seconds } = this.state;
-		const daysRadius = mapNumber(days, 30, 0, 0, 360);
-		const hoursRadius = mapNumber(hours, 24, 0, 0, 360);
-		const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
-		const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
+      this.setState({ days, hours, minutes, seconds });
+    }, 1000);
+  }
 
-		if(!seconds) {
-			return null;
-		}
-		
-		return (
-			<div>
-				<h1 className='h1countdown'>Countdown</h1>
-				<div className='countdown-wrapper'>
-					{days && (
-						<div className='countdown-item'>
-							<SVGCircle radius={daysRadius} />
-							{days} 
-							<span>days</span>
-						</div>
-					)}
-					{hours && (
-						<div className='countdown-item'>							
-							<SVGCircle radius={hoursRadius} />
-							{hours} 
-							<span>hours</span>
-						</div>
-					)}
-					{minutes && (
-						<div className='countdown-item'>
-							<SVGCircle radius={minutesRadius} />
-							{minutes} 
-							<span>minutes</span>
-						</div>
-					)}
-					{seconds && (
-						<div className='countdown-item'>
-							<SVGCircle radius={secondsRadius} />
-							{seconds} 
-							<span>seconds</span>
-						</div>
-					)}
-				</div>
-			</div>
-		);
-	}
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  render() {
+    const { days, hours, minutes, seconds } = this.state;
+    const daysRadius = mapNumber(days, 30, 0, 0, 360);
+    const hoursRadius = mapNumber(hours, 24, 0, 0, 360);
+    const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
+    const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
+
+    if (!seconds) {
+      return null;
+    }
+
+    return (
+      <div>
+        <h1 className='h1countdown'>Countdown</h1>
+        <div className='countdown-wrapper'>
+          {days && (
+            <div className='countdown-item'>
+              <SVGCircle radius={daysRadius} />
+              {days}
+              <span>days</span>
+            </div>
+          )}
+          {hours && (
+            <div className='countdown-item'>
+              <SVGCircle radius={hoursRadius} />
+              {hours}
+              <span>hours</span>
+            </div>
+          )}
+          {minutes && (
+            <div className='countdown-item'>
+              <SVGCircle radius={minutesRadius} />
+              {minutes}
+              <span>minutes</span>
+            </div>
+          )}
+          {seconds && (
+            <div className='countdown-item'>
+              <SVGCircle radius={secondsRadius} />
+              {seconds}
+              <span>seconds</span>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 const SVGCircle = ({ radius }) => (
-	<svg className='countdown-svg'>
-		<path fill="none" stroke="#00b100" stroke-width="4" d={describeArc(50, 50, 48, 0, radius)}/>
-	</svg>
+  <svg className='countdown-svg'>
+    <path fill='none' stroke='#00b100' strokeWidth='4' d={describeArc(50, 50, 48, 0, radius)} />
+  </svg>
 );
 
-
-
-// From stackoverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+  var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
 
   return {
-    x: centerX + (radius * Math.cos(angleInRadians)),
-    y: centerY + (radius * Math.sin(angleInRadians))
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians)
   };
 }
 
-function describeArc(x, y, radius, startAngle, endAngle){
+function describeArc(x, y, radius, startAngle, endAngle) {
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
 
-    var start = polarToCartesian(x, y, radius, endAngle);
-    var end = polarToCartesian(x, y, radius, startAngle);
+  var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
 
-    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+  var d = ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y].join(' ');
 
-    var d = [
-        "M", start.x, start.y, 
-        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
-    ].join(" ");
-
-    return d;       
+  return d;
 }
 
-// Stackoverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 function mapNumber(number, in_min, in_max, out_min, out_max) {
-  return (number - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  return ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 }
 
 const TimeClock = () => {
-    return (
-        <div>
-            <Countdown 
-        timeTillDate="05 12 2024, 6:00 am" 
-        timeFormat="MM DD YYYY, h:mm a" 
-    />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Countdown timeTillDate={moment().add(365, 'days').format('MM DD YYYY, h:mm a')} timeFormat='MM DD YYYY, h:mm a' />
+    </div>
+  );
+};
 
-
-export default TimeClock
+export default TimeClock;
