7be8d7f1f22a0aa4aabcac870fa6c3c07c43a83d
blackalien <jsmobst@gmail.com>
2023-08-29 19:30:57 -0500

    styling 32 no cobot

--- src/components/CoBot/widgets/MarketNews.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MarketNews from '../widgets/Market_News';
-const MarketNewsIndex = () => {
-  return (
-    <MarketNews/>
-  );
-};
-
-export default MarketNewsIndex;
