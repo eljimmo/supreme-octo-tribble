3f8e51e18dc66c97ceb83c45b2df397b5d4e0e8f
blackalien <jsmobst@gmail.com>
2023-09-02 21:08:37 -0500

    thuggish ruggish bones npm version added 4

--- src/components/InfoSection/Index_Infosection2.js
+++ src/components/InfoSection/Index_Infosection2.js
@@ -7,15 +7,12 @@ import {
   TopLine,
   Heading,
   Subtitle,
-  BtnWrap,
 
 } from './InfoElements';
 import { Canvas, useThree, extend, useFrame } from '@react-three/fiber'
 import { useAspect, useVideoTexture, Scroll, Image, ScrollControls, useIntersect, useTexture, useCursor, Effects as EffectComposer } from '@react-three/drei'
 
-import { useSphere, Physics } from '@react-three/cannon'
 import * as THREE from "three"
-import { AsciiEffect } from 'three-stdlib'
 
 
 import { SSAOPass } from "three-stdlib"
@@ -50,145 +47,9 @@ function FallbackMaterial({ url }) {
 }
 
 
-
-function Clump({ mat = new THREE.Matrix4(), vec = new THREE.Vector3(), ...props }) {
-    const texture = useTexture("/crosshair.jpg")
-    const [ref, api] = useSphere(() => ({ args: [1], mass: 1, angularDamping: 0.1, linearDamping: 0.65, position: [rfs(20), rfs(20), rfs(20)] }))
-    useFrame((state) => {
-      for (let i = 0; i < 40; i++) {
-        // Get current whereabouts of the instanced sphere
-        ref.current.getMatrixAt(i, mat)
-        // Normalize the position and multiply by a negative force.
-        // This is enough to drive it towards the center-point.
-        api.at(i).applyForce(vec.setFromMatrixPosition(mat).normalize().multiplyScalar(-50).toArray(), [0, 0, 0])
-      }
-    })
-    return <instancedMesh ref={ref} castShadow receiveShadow args={[null, null, 40]} geometry={sphereGeometry} material={baubleMaterial} material-map={texture} />
-  }
   
-  function Pointer() {
-    const viewport = useThree((state) => state.viewport)
-    const [, api] = useSphere(() => ({ type: "Kinematic", args: [3], position: [0, 0, 0] }))
-    return useFrame((state) => api.position.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0))
-  }
-  
-  function Effects(props) {
-    const { scene, camera } = useThree()
-    return (
-      <EffectComposer {...props}>
-        <sSAOPass args={[scene, camera, 100, 100]} kernelRadius={1.2} kernelSize={0} />
-      </EffectComposer>
-    )
-  }
   
 
-
-  function Torusknot(props) {
-    const ref = useRef()
-    const [clicked, click] = useState(false)
-    const [hovered, hover] = useState(false)
-    useCursor(hovered)
-    useFrame((state, delta) => (ref.current.rotation.x = ref.current.rotation.y += delta / 2))
-    return (
-      <mesh
-        {...props}
-        ref={ref}
-        scale={clicked ? 1.5 : 1.25}
-        onClick={() => click(!clicked)}
-        onPointerOver={() => hover(true)}
-        onPointerOut={() => hover(false)}>
-        <torusKnotGeometry args={[1, 0.2, 128, 32]} />
-      </mesh>
-    )
-  }
-  
-  function AsciiRenderer({
-    renderIndex = 1,
-    bgColor = 'black',
-    fgColor = 'white',
-    characters = ' .:-+*=%@#',
-    invert = true,
-    color = false,
-    resolution = 0.15
-  }) {
-    // Reactive state
-    const { size, gl, scene, camera } = useThree()
-  
-    // Create effect
-    const effect = useMemo(() => {
-      const effect = new AsciiEffect(gl, characters, { invert, color, resolution })
-      effect.domElement.style.position = 'absolute'
-      effect.domElement.style.top = '0px'
-      effect.domElement.style.left = '0px'
-      effect.domElement.style.pointerEvents = 'none'
-      return effect
-    }, [characters, invert, color, resolution])
-  
-    // Styling
-    useLayoutEffect(() => {
-      effect.domElement.style.color = fgColor
-      effect.domElement.style.backgroundColor = bgColor
-    }, [fgColor, bgColor])
-  
-    // Append on mount, remove on unmount
-    useEffect(() => {
-      gl.domElement.style.opacity = '0'
-      gl.domElement.parentNode.appendChild(effect.domElement)
-      return () => {
-        gl.domElement.style.opacity = '1'
-        gl.domElement.parentNode.removeChild(effect.domElement)
-      }
-    }, [effect])
-  
-    // Set size
-    useEffect(() => {
-      effect.setSize(size.width, size.height)
-    }, [effect, size])
-  
-    // Take over render-loop (that is what the index is for)
-    useFrame((state) => {
-      effect.render(scene, camera)
-    }, renderIndex)
-  
-    // This component returns nothing, it is a purely logical
-  }
-  
-  
-  
- 
-function Item({ url, scale, ...props }) {
-  const visible = useRef(false)
-  const [hovered, hover] = useState(false)
-  const ref = useIntersect((isVisible) => (visible.current = isVisible))
-  const { height } = useThree((state) => state.viewport)
-  useFrame((state, delta) => {
-    ref.current.position.y = THREE.MathUtils.damp(ref.current.position.y, visible.current ? 0 : -height / 2 + 1, 4, delta)
-    ref.current.material.zoom = THREE.MathUtils.damp(ref.current.material.zoom, visible.current ? 1 : 1.5, 4, delta)
-    ref.current.material.grayscale = THREE.MathUtils.damp(ref.current.material.grayscale, hovered ? 0 : 1, 4, delta)
-  })
-  return (
-    <group {...props}>
-      <Image ref={ref} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} scale={scale} url={url} />
-    </group>
-  )
-}
-
-function Items() {
-  const { width: w, height: h } = useThree((state) => state.viewport)
-  return (
-    <Scroll>
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 6, 0, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[2, w / 3, 1]} position={[w / 30, -h, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 1, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 5, w / 5, 1]} position={[w / 4, -h * 1.2, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 5, w / 5, 1]} position={[w / 10, -h * 1.75, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 4, -h * 2, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 2.6, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 2, w / 2, 1]} position={[w / 4, -h * 3.1, 0]} />
-      <Item url="c4cA8UN.jpg" scale={[w / 2.5, w / 2, 1]} position={[-w / 6, -h * 4.1, 0]} />
-    </Scroll>
-  )
-} 
 
 const InfoSection2 = ({
   lightBg,
