e6d5bb1a8ed1e96ca645c0abb0c67bdbbe88f7b6
blackalien <jsmobst@gmail.com>
2023-12-11 13:00:07 -0600

    make it better 776

--- src/pages/Service_Index/Login.js
+++ src/pages/Service_Index/Login.js
@@ -14,7 +14,7 @@ import styled from 'styled-components';
   color: #fff;
   background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#010606')};
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 768px) { 
     padding: 100px 0;
   }
 `;
