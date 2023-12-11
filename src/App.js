6b8267d332d5de63cf02ac47aade48147d810f34
blackalien <jsmobst@gmail.com>
2023-12-11 13:09:54 -0600

    no login

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import GeneralTermsIndex from "./pages/Service_Index/Generalterms";
 import Howitworks from "./pages/Service_Index/How";
 import SupportPage from "./pages/Service_Index/Support";
 import ChooseUs from "./pages/Service_Index/WhyLeibniz";
-import Login from "./pages/Service_Index/login";
+// import Login from "./pages/Service_Index/login";
 import Signup from "./pages/Service_Index/Signup";
 
 export default function App() {
@@ -33,7 +33,7 @@ export default function App() {
             <Route path="/signup" element={<Signup/>}/>
     
     
-            <Route path="/login" element={<Login/>}/>
+            {/* <Route path="/login" element={<Login/>}/> */}
 
     </Routes>
 
