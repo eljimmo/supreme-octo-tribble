d6023419abf4a73e11da42be0df17932374809aa
blackalien <jsmobst@gmail.com>
2023-12-12 17:20:14 -0600

    lesser packages, trying to make smaller build slug size

--- src/pages/Service_Index/OptimizerChart.js
+++ src/pages/Service_Index/OptimizerChart.js
@@ -1,94 +1,94 @@
-import React from 'react';
-import Plot from 'react-plotly.js';
+// import React from 'react';
+// import Plot from 'react-plotly.js';
 
-const OptimizerComparisonChart = ({ sgdData, adamData, rmspropData, adagradData }) => {
-
-
-    const sgdTrace = {
-        x: sgdData.epochs,
-        y: sgdData.loss,
-        mode: 'lines',
-        name: 'SGD'
-    };
-
-    const adamTrace = {
-        x: adamData.epochs,
-        y: adamData.loss,
-        mode: 'lines',
-        name: 'Adam'
-    };
-
-    const rmspropTrace = {
-        x: rmspropData.epochs,
-        y: rmspropData.loss,
-        mode: 'lines',
-        name: 'RMSprop'
-    };
-
-    const adagradTrace = {
-        x: adagradData.epochs,
-        y: adagradData.loss,
-        mode: 'lines',
-        name: 'Adagrad'
-    };
+// const OptimizerComparisonChart = ({ sgdData, adamData, rmspropData, adagradData }) => {
 
 
+//     const sgdTrace = {
+//         x: sgdData.epochs,
+//         y: sgdData.loss,
+//         mode: 'lines',
+//         name: 'SGD'
+//     };
 
+//     const adamTrace = {
+//         x: adamData.epochs,
+//         y: adamData.loss,
+//         mode: 'lines',
+//         name: 'Adam'
+//     };
 
-    // const data = [sgdTrace, adamTrace, rmspropTrace, adagradTrace];
+//     const rmspropTrace = {
+//         x: rmspropData.epochs,
+//         y: rmspropData.loss,
+//         mode: 'lines',
+//         name: 'RMSprop'
+//     };
+
+//     const adagradTrace = {
+//         x: adagradData.epochs,
+//         y: adagradData.loss,
+//         mode: 'lines',
+//         name: 'Adagrad'
+//     };
 
 
 
-  const isMobile = window.innerWidth <= 600;
-  const fontSize = isMobile ? 5 : 14;  // Reduced the mobile font size to 8
-  const legendSize = isMobile ? 5 : 11;
 
-  const legendPlacement = isMobile ? 
-    { orientation: 'h', x: 0, y: -0.2, font: { size: legendSize } } :  {};
+//     // const data = [sgdTrace, adamTrace, rmspropTrace, adagradTrace];
 
-  // Adjust the width and height based on your requirements
-  const chartSize = isMobile ? { width: 275, height: 175 } : {};
 
-  const layout = {
-    ...chartSize, 
-    title: {
-      text: 'Optimizer Loss Comparison',
-      font: {
-        size: fontSize
-      }
-    },
-    xaxis: {
-      title: {
-        text: 'Epochs',
-        font: {
-          size: fontSize
-        }
-      }
-    },
-    yaxis: {
-      title: {
-        text: 'Loss',
-        font: {
-          size: fontSize
-        }
-      }
-    },
-    legend: {
-      ...legendPlacement
-    },
-    paper_bgcolor: 'rgba(0,0,0,0)',
-    plot_bgcolor: 'rgba(0,0,0,0)',
-    autosize: true,
-    margin: {
-      l: 50,
-      r: 50,
-      b: 50,
-      t: 50,
-      pad: 4
-    }
-  };
 
-  return <Plot data={[sgdTrace, adamTrace, rmspropTrace, adagradTrace]} layout={layout} responsive={true} />;
-}
+//   const isMobile = window.innerWidth <= 600;
+//   const fontSize = isMobile ? 5 : 14;  // Reduced the mobile font size to 8
+//   const legendSize = isMobile ? 5 : 11;
 
-export default OptimizerComparisonChart;
+//   const legendPlacement = isMobile ? 
+//     { orientation: 'h', x: 0, y: -0.2, font: { size: legendSize } } :  {};
+
+//   // Adjust the width and height based on your requirements
+//   const chartSize = isMobile ? { width: 275, height: 175 } : {};
+
+//   const layout = {
+//     ...chartSize, 
+//     title: {
+//       text: 'Optimizer Loss Comparison',
+//       font: {
+//         size: fontSize
+//       }
+//     },
+//     xaxis: {
+//       title: {
+//         text: 'Epochs',
+//         font: {
+//           size: fontSize
+//         }
+//       }
+//     },
+//     yaxis: {
+//       title: {
+//         text: 'Loss',
+//         font: {
+//           size: fontSize
+//         }
+//       }
+//     },
+//     legend: {
+//       ...legendPlacement
+//     },
+//     paper_bgcolor: 'rgba(0,0,0,0)',
+//     plot_bgcolor: 'rgba(0,0,0,0)',
+//     autosize: true,
+//     margin: {
+//       l: 50,
+//       r: 50,
+//       b: 50,
+//       t: 50,
+//       pad: 4
+//     }
+//   };
+
+//   return <Plot data={[sgdTrace, adamTrace, rmspropTrace, adagradTrace]} layout={layout} responsive={true} />;
+// }
+
+// export default OptimizerComparisonChart;
