4d9f23b6555b8d12caec09c1ea17eb44b2c4e71d
blackalien <jsmobst@gmail.com>
2023-12-19 17:47:53 -0600

    lets c 4566

--- src/pages/Service_Index/Welcome_page_index.js
+++ src/pages/Service_Index/Welcome_page_index.js
@@ -3,18 +3,18 @@
   // import Videoapp from '../../components/VideoTexture/App';
   import HeroSection from '../../components/HeroSection/index';
   import ScApp from '../../components/ScrollControl/src/App';
-  import ScrollBelt from  "../../components/portfolio_react/components/about/ScrollBeltIndex";
-  import HeroMachineLearning from "../../components/portfolio_react/components/hero/hero.machinelearning";
-  import Experience from "../../components/MachineLearning/src/components/experience/Expreience";
-  import Underlay from '../../components/Landing/Underlay';
-  import App from '../../components/Curl_Noise/App';
-  import TimeClock from '../../components/TimeClock/Index';
-  import Heroartificialintelligence from "../../components/portfolio_react/components/hero/hero.artificialintelligence";
+  // import ScrollBelt from  "../../components/portfolio_react/components/about/ScrollBeltIndex";
+  // import HeroMachineLearning from "../../components/portfolio_react/components/hero/hero.machinelearning";
+  // import Experience from "../../components/MachineLearning/src/components/experience/Expreience";
+  // import Underlay from '../../components/Landing/Underlay';
+  // import App from '../../components/Curl_Noise/App';
+  // import TimeClock from '../../components/TimeClock/Index';
+  // import Heroartificialintelligence from "../../components/portfolio_react/components/hero/hero.artificialintelligence";
   import styled from 'styled-components';
   import Navbar from '../../components/Navbar';
   import Sidebar from '../../components/Sidebar/index';
-  import BackgroundApp from '../../components/FrostedGlass/heroindex';
-  import MachineApp from '../../components/FrostedGlass/MachineIndex';
+  // import BackgroundApp from '../../components/FrostedGlass/heroindex';
+  // import MachineApp from '../../components/FrostedGlass/MachineIndex';
 
 
   const HeroContainerTransparent = styled.div`
