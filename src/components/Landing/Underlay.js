91ff54c184cb647d3cfcb877c72b062d58edabf5
blackalien <jsmobst@gmail.com>
2023-12-20 13:33:06 -0600

    gold & dollars, double alpha, triple alpha 3

--- src/components/Landing/Underlay.js
+++ src/components/Landing/Underlay.js
@@ -2,25 +2,6 @@ import styled, { keyframes } from 'styled-components';
 import React from 'react';
 import TimeClock from '../TimeClock/Index';
 
-
-const HeroP = styled.p`
-  margin-top: 24px;
-  color: #fff;
-  font-size: 24px;
-  text-align: center;
-  max-width: 600px;
-  display: inline-block;
-
-  @media screen and (max-width: 768px) {
-    font-size: 24px;
-  }
-
-  @media screen and (max-width: 480px) {
-    font-size: 18px;
-  }
-`;
-
-
 const Grid = styled.div`
   position: absolute;
   width: 100%;
@@ -62,8 +43,13 @@ const Left = styled.div`
 `
 
 const Right = styled.div`
-  position: relative;
+  position: absolute;
   display: flex;
+  top: ${(props) => (props.vertical ? '0px' : '10%')};
+  right: ${(props) => (props.vertical ? '25%' : '0px')};
+  width: ${(props) => (props.vertical ? '2px' : '150px')};
+  height: ${(props) => (props.vertical ? '150px' : '2px')};
+
 
   background: transparent;
   
@@ -77,128 +63,9 @@ const Sub = styled.div`
 `;
 
 
-const FeatureBox = styled.div`
-  flex: 1;
-  background-color: transparent;
-  padding: 20px;
-  border-radius: 10px;
-  margin: 0 10px;
-  border: 2px solid #00b100;  // Green border
-
-  &:first-child {
-    margin-left: 0;
-  }
-
-  &:last-child {
-    margin-right: 0;
-  }
-
-  // Hover effect
-  &:hover {
-    transform: scale(1.05);  // Slightly enlarges the card on hover
-    box-shadow: 0px 4px 15px rgba(0, 177, 0, 0.4);  // Greenish shadow
-    transition: all 0.3s ease-out;  // Smooth transition effect
-  }
-`;
 
 
 
-
-
-
-
-const TopLine1 = styled.div`
-  color: #00b100;
-  font-size: 12px;
-  line-height: 16px;
-  font-weight: 700;
-  letter-spacing: 1.4px;
-  text-transform: uppercase;
-  margin-bottom: 16px;
-`;
-
-const gradientText = keyframes`
-{
-  0% {
-    background-position: 0 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0 50%;
-  }
-}`;
-
-const Title = styled.h1`
-  animation: ${gradientText} 5s ease-in-out infinite;
-  font-size: 48px;
-  letter-spacing: 0.8rem;
-  text-transform: uppercase;
-  text-align: center;
-  background: linear-gradient(to right, #ee9ca7, #ffdde1, #2193b0, #6dd5ed);
-  background-size: 200%;
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-
-  @media screen and (max-width: 480px) {
-    font-size: 32px;
-  }
-`;
-
-
-const PageWrapper = styled.div`
-  display: relative;
-  max-width: 800px;
-  width: 100%;  // take full width by default
-  margin: 20px auto;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-
-  @media screen and (max-width: 768px) {
-    padding: 10px;  // Reduce padding for mobile
-  }
-`;
-
-
-const Content = styled.div`
-  background-color: transparent;
-  padding: 20px;
-  border-radius: 10px;
-  margin-top: 20px;
-  text-align: center;
-`;
-
-const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 40px;
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;  // stack boxes vertically on mobile
-  }
-`;
-
-
-
-const Subtitle = styled.h2`
-  font-size: 28px;
-  color: #2193b0;
-  margin-top: 30px;
-  text-align: center;
-`;
-
-const Paragraph = styled.p`
-  font-size: 16px;
-  line-height: 1.6;
-  margin-bottom: 20px;
-  color: #ffffff;
-  @media screen and (max-width: 480px) {
-    font-size: 14px;  // slightly smaller font for mobile
-  }
-`;
 
 const StyledParagraph = styled.p`
   font-size: 16px;
