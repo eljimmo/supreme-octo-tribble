7ddcdd87560506f16b91c0cdd87e021ed1dee3ef
blackalien <jsmobst@gmail.com>
2023-12-20 12:53:26 -0600

    gold & dollars, double alpha, triple alpha 5

--- src/components/Curl_Noise/App.js
+++ src/components/Curl_Noise/App.js
@@ -9,10 +9,10 @@ import { EffectComposer, Bloom, LUT, BrightnessContrast, HueSaturation } from '@
 export default function App() {
   const props = {
     focus: 5.1,
-    speed: 42.9,
-    aperture: 5.8,
+    speed: 100,
+    aperture: 1.8,
     fov: 50,
-    curl: 0.65
+    curl: 0.25
   };
 
   return (
